0ebff75efd56c93f698d0db68d0e1f2943c5b5c3
yvonnebarker <Ydbarker@gmail.com>
2017-01-16 17:19:25 +0000

    Modified to print a number before the name of each student

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]}: #{student[:cohort]} cohort"
   end
 end
 
